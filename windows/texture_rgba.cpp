a458d3a0d9688276a9baeb614b06e86735d2e664
fufesou <shuanglongchen@yeah.net>
2023-02-20 23:02:22 +0800

    better lock guard
    Signed-off-by: fufesou <shuanglongchen@yeah.net>

--- windows/texture_rgba.cpp
+++ windows/texture_rgba.cpp
@@ -19,17 +19,20 @@ TextureRgba::~TextureRgba()
 void TextureRgba::MarkVideoFrameAvailable(
 	std::vector<uint8_t> &buffer, size_t width, size_t height)
 {
-	const std::lock_guard<std::mutex> lock(mutex_);
-	if (last_fg_index_ != fg_index_)
+	int bg_index = 0;
 	{
-		flutter_pixel_buffer_.buffer = static_cast<const uint8_t *>(buffer_tmp_[fg_index_].data());
-		flutter_pixel_buffer_.width = width_[fg_index_];
-		flutter_pixel_buffer_.height = height_[fg_index_];
-		last_fg_index_ = fg_index_;
-		texture_registrar_->MarkTextureFrameAvailable(texture_id_);
+		const std::lock_guard<std::mutex> lock(mutex_);
+		if (last_fg_index_ != fg_index_)
+		{
+			flutter_pixel_buffer_.buffer = static_cast<const uint8_t *>(buffer_tmp_[fg_index_].data());
+			flutter_pixel_buffer_.width = width_[fg_index_];
+			flutter_pixel_buffer_.height = height_[fg_index_];
+			last_fg_index_ = fg_index_;
+			texture_registrar_->MarkTextureFrameAvailable(texture_id_);
+		}
+		bg_index = fg_index_ ^ 1;
 	}
 
-	int bg_index = fg_index_ ^ 1;
 	buffer.swap(buffer_tmp_[bg_index]);
 	width_[bg_index] = width;
 	height_[bg_index] = height;
